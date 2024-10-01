f865a531afa473068c0de9c29338e64a988b7537
JuanEs-az <juarangoz@unal.edu.co>
2024-10-01 13:13:22 -0500

    initial commit

---
+++ src/utils/fonts.ts
@@ -0,0 +1,10 @@
+import { Montserrat, Jacquard_24 } from "next/font/google";
+export const montserrat = Montserrat({
+  weight: ["400"],
+  subsets: ['latin']
+
+});
+export const jacquard = Jacquard_24({
+  weight: ["400"],
+  subsets: ['latin']
+});
